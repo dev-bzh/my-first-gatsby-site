cdae531df5b051d4eb9a6889927269c2fd5ecb0f
tom <devlop.bzh@gmail.com>
2022-10-23 17:58:31 +0200

    gatsby tuto part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,29 +7,32 @@ const BlogPage = ({data}) => {
     //data = query (requete graphql de la page transmise en tant que prop data, doit être défini en dehors du composant de page)
     return (
         <Layout pageTitle="My blog posts">
-            
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
-
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
+query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
+  }
 `
 
 export const Head = () => <Seo title="Blog" />
